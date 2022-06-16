db2f846e65133962a95d18273596f5c1108efd97
unknown <aalwa049@uottawa.ca>
2022-06-16 16:27:58 -0400

    update-recipe-model-to-include-ingredients

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 import { Recipe } from './recipe.model';
 
 export class RecipeService {
@@ -7,12 +8,22 @@ export class RecipeService {
     new Recipe(
       'Poutine',
       'Amazing poutine',
-      'https://media.blogto.com/articles/97f2-20150127-chrisjerk590-04.jpg?w=2048&cmd=resize_then_crop&height=1365&quality=70'
+      'https://media.blogto.com/articles/97f2-20150127-chrisjerk590-04.jpg?w=2048&cmd=resize_then_crop&height=1365&quality=70',
+      [
+        new Ingredient('Chicken', 1),
+        new Ingredient('Beef', 1),
+        new Ingredient('Cheese', 3),
+      ]
     ),
     new Recipe(
       'Poutine again!',
       'Amazing poutine',
-      'https://media.blogto.com/articles/97f2-20150127-chrisjerk590-04.jpg?w=2048&cmd=resize_then_crop&height=1365&quality=70'
+      'https://media.blogto.com/articles/97f2-20150127-chrisjerk590-04.jpg?w=2048&cmd=resize_then_crop&height=1365&quality=70',
+      [
+        new Ingredient('Chicken', 1),
+        new Ingredient('Beef', 1),
+        new Ingredient('Cheese', 3),
+      ]
     ),
   ];
 
